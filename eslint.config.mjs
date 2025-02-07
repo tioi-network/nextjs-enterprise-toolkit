5b18837303f394b602ad4b103c306bbcedaa4961
N <beforeoafterm@gmail.com>
2025-02-08 03:10:50 +0800

    chore: load the tailwind plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,4 @@
-// https://github.com/francoismassart/eslint-plugin-tailwindcss/pull/381
-// import eslintPluginTailwindcss from "eslint-plugin-tailwindcss"
+import eslintPluginTailwindcss from "eslint-plugin-tailwindcss"
 import eslintPluginNext from "@next/eslint-plugin-next"
 import eslintPluginImport from "eslint-plugin-import"
 import eslintPluginStorybook from "eslint-plugin-storybook"
@@ -23,8 +22,7 @@ const config = typescriptEslint.config(
     ignores: eslintIgnore,
   },
   ...eslintPluginStorybook.configs["flat/recommended"],
-  //  https://github.com/francoismassart/eslint-plugin-tailwindcss/pull/381
-  // ...eslintPluginTailwindcss.configs["flat/recommended"],
+  ...eslintPluginTailwindcss.configs["flat/recommended"],
   typescriptEslint.configs.recommended,
   eslintPluginImport.flatConfigs.recommended,
   {
