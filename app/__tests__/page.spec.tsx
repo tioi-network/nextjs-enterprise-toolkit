9120e1d9a72317d8a647faf6490bb159d3cfad72
N <beforeoafterm@gmail.com>
2025-02-08 23:16:37 +0800

    test: root page

---
+++ app/__tests__/page.spec.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react"
+import { beforeEach, describe, expect, it, vi } from "vitest"
+import Page from "../page"
+
+import "@testing-library/jest-dom"
+
+// Mock auth0 functions
+vi.mock("@auth0/nextjs-auth0", () => ({
+  withPageAuthRequired: (component: any) => component,
+  getSession: vi.fn(),
+}))
+
+describe("RootPage", () => {
+  beforeEach(() => {
+    vi.clearAllMocks()
+  })
+
+  it("should render the heading", async () => {
+    render(await Page({}))
+    const heading = screen.getByRole("heading", { level: 1 })
+    expect(heading).toBeInTheDocument()
+  })
+})
